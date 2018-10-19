6d0453e542573faee6af92d1a8ebf184cbf75155
JupyterHub User (dsperka) <dsperka@vmp618.vampire>
2018-10-19 18:16:01 +0000

    update intro

--- combine_tutorials_2018/combine_intro/run_hgg_pvalue.sh
+++ combine_tutorials_2018/combine_intro/run_hgg_pvalue.sh
@@ -3,9 +3,9 @@
 MASS=115
 while [  $MASS -lt 146 ]; do
     echo Computing p-value for MH = $MASS
-    #combine -n SignifExp -M ProfileLikelihood --signif -m $MASS hgg_8TeV_MVA_cat0145.root -t -1 --expectSignal=1 --toysFreq --pvalue > expsignif$MASS.txt
-    combine -n SignifExp -M ProfileLikelihood --signif -m $MASS hgg_8TeV_MVA_cat0145.root -t -1 --expectSignal=1 --pvalue > expsignif$MASS.txt
-    combine -n SignifObs -M ProfileLikelihood --signif -m $MASS hgg_8TeV_MVA_cat0145.root --pvalue > obssignif$MASS.txt
+    #combine -n SignifExp -M Significance -m $MASS hgg_8TeV_MVA_cat0145.root -t -1 --expectSignal=1 --toysFreq --pvalue > expsignif$MASS.txt
+    combine -n SignifExp -M Significance -m $MASS hgg_8TeV_MVA_cat0145.root -t -1 --expectSignal=1 --pvalue > expsignif$MASS.txt
+    combine -n SignifObs -M Significance -m $MASS hgg_8TeV_MVA_cat0145.root --pvalue > obssignif$MASS.txt
     let MASS=MASS+1 
 done
 
