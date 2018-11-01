047b173679a70a8925735ca2f8e0c54a32c0febf
cms.dsperka <cms.dsperka@vmp647.vampire>
2018-11-01 15:50:01 +0000

    fix custom crab script

--- combine_tutorials_2018/htt_slim/custom_crab.py
+++ combine_tutorials_2018/htt_slim/custom_crab.py
@@ -1,6 +1,7 @@
 def custom_crab(config):
   print '>> Customising the crab config'
-  from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+  config.JobType.allowUndistributedCMSSW = True
   config.Site.storageSite = 'T3_US_FNALLPC'
   #config.Site.blacklist = ['T3_US_Baylor','T2_EE_Estonia']
-  config.Data.outLFNDirBase = '/store/user/%s/Combine_Tutorial_2018/' % (getUsernameFromSiteDB())
+  config.Data.outLFNDirBase = '/store/user/dsperka/Combine_Tutorial_2018/'
+  # YOU WILL NEED TO REPLACE MY USER NAME WITH YOUR OWN!!!
